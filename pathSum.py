0cdaa22a36442f0505b11d4cf7e6996af03a5240
coderwyc <ucaswyc@126.com>
2015-01-06 20:26:08 +0800

    Update pathSum.py

--- pathSum.py
+++ pathSum.py
@@ -31,7 +31,21 @@ class Solution:
         elif root.val == sum and root.left is None and root.right is None: # in leaf node satisfy the condition
             return True
         else:
-            if self.hasPathSum(root.left, sum - root.val): # check to see left child is satisfy
-                return True
-            else:
-                return self.hasPathSum(root.right, sum - root.val) # otherwise check to see left child is satisfy
+            # Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @param sum, an integer
+    # @return a boolean
+    def hasPathSum(self, root, sum):
+        if root is None:
+            return False
+        elif root.val == sum and root.left is None and root.right is None: # in leaf node satisfy the condition
+            return True
+        else: # check to see either left or right child is satisfyed
+            return self.hasPathSum(root.left, sum - root.val) or  self.hasPathSum(root.right, sum - root.val)
